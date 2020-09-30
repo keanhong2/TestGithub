906c473d19132f4d4f0b94046e6e0caf6f1e4929
keanhong2 <keanhong2@yahoo.com>
2020-10-01 04:05:57 +0800

    Changes

--- TestGithub/Program.cs
+++ TestGithub/Program.cs
@@ -4,9 +4,10 @@ namespace TestGithub
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("here is a change!");
         }
     }
 }
