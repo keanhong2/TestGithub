a35e143b5d3a8acfc5d5cd4b76c3345ae48c5732
keanhong2 <keanhong2@yahoo.com>
2020-10-01 04:08:52 +0800

    2nd changes

--- TestGithub/Program.cs
+++ TestGithub/Program.cs
@@ -8,6 +8,7 @@ namespace TestGithub
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("here is a change!");
+            Console.WriteLine("here is 2nd changes");
         }
     }
 }
